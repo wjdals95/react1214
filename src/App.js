5859cff00b73ac7bcb7a771e24f0f675c15b0d87
4thdraw <4thdraw2@gmail.com>
2022-12-14 15:55:32 +0900

    소개페이지 컨텐츠컴포넌트제작

--- src/App.js
+++ src/App.js
@@ -1,13 +1,103 @@
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="./img/logo.svg" className="img-fluid w-100" alt="logo" />
+      <header className="App-header d-flex justify-content-between align-items-center px-lg-5">
+        <h1 className="mb-0">
+          <a href="" className="d-block">
+            <img src="./img/logo.svg" className="img-fluid w-100" alt="logo" />
+          </a>
+        </h1>
+        <Navi></Navi>        
       </header>
+      <Sliderswiper></Sliderswiper>
+      <Preinterview></Preinterview>
+      <Footer></Footer>
     </div>
   );
+}
+
+function Navi(){
+  const naviDb = [{
+                    naviText : "포트폴리오",
+                    naviLink : "#portfolio"
+                  },
+                  {
+                    naviText : "인물탐방",
+                    naviLink : "#me"
+                                  },
+                  {
+                    naviText : "사전인터뷰 및 제안",
+                    naviLink : "#preInterview"
+                                  }
+                  ]
+  return(
+    <ul className="d-flex ">
+      {
+        naviDb.map((item, index) =>{
+          return(
+            <li id={ 'naivID'+index }><a href={item.naviLink} className="text-decoration-none">{item.naviText}</a></li>
+          )
+        }
+        )
+          
+      }
+    </ul>
+  )
+}
+
+const Sliderswiper = () => {
+ 
+    return (
+      <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+      autoplay={{
+        "delay": 2500,
+        "disableOnInteraction": false
+      }}
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+
+      
+    >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>     
+      </Swiper>
+    );
+ 
+}
+const Preinterview = () => {
+  return(
+    <section className="section py-5 text-center">
+        <h3>규칙위에 틀을 깨는 개발자가 되겠습니다.</h3>
+        <div className="py-5"></div>
+    </section>
+  )
+}
+const Footer = () =>{
+  return(
+    <footer className="py-5 border-top">
+      <ul className="d-flex justify-content-center">
+        <li><a href="">git</a></li>
+        <li><a href="">instar</a></li>
+        <li><a href="">notion</a></li>
+      </ul>
+    </footer>
+  )
 }
 
 export default App;
