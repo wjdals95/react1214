894f6f6c7f2153daaac27fc45a4f28177823970d
wjdals95 <wjdals992@naver.com>
2022-12-14 17:59:26 +0900

    1214 슬라이드배너 연습

--- src/App.js
+++ src/App.js
@@ -56,27 +56,24 @@ const Sliderswiper = () => {
  
     return (
       <Swiper
-      // install Swiper modules
       modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
       spaceBetween={50}
-      slidesPerView={3}
-      navigation
+      slidesPerView={1}
       autoplay={{
-        "delay": 2500,
-        "disableOnInteraction": false
+        delay: 2500,
+        disableOnInteraction: false,
       }}
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
-
-      
+      onSwiper={(swiper) => console.log(swiper)}
     >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>     
-      </Swiper>
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
     );
  
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/layout.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './css/layout.scss';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
